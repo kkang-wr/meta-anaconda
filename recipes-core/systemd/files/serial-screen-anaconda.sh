cb790ede66e74ba7174f31669892831f5d979aaa
Hongxu Jia <hongxu.jia@windriver.com>
2019-07-05 04:15:39 -0400

    serial-screen-anaconda.sh: drop unused echo
    Since commit [fd4209b systemd-serialgetty.bbappend: add
    var-INSTALLER_SERIAL] applied, it is not necessary to
    echo root to serial console
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- recipes-core/systemd/files/serial-screen-anaconda.sh
+++ recipes-core/systemd/files/serial-screen-anaconda.sh
@@ -1,5 +1,5 @@
 # Detect if it is serial tty, if yes, we need to invoke
 # screen to attach anaconda
 console=`/usr/bin/tty`
-[ "${console##/dev/ttyS}" != "${console}" ] && echo " login:" && /usr/bin/screen -x anaconda-init
+[ "${console##/dev/ttyS}" != "${console}" ] && /usr/bin/screen -x anaconda-init
 
